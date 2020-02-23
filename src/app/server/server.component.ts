0b5f0f6e4aa4dc369d7b0e834d5f7135f0b9e88d
Dominick Dufner <dominickdufner@yahoo.com>
2020-02-23 13:37:50 -0600

    pushing files

---
+++ src/app/server/server.component.ts
@@ -0,0 +1,7 @@
+import {Component} from '@angular/core';
+
+@Component({
+    selector: 'app-server', //used to connect to a specific html tag like 'app-root'
+    templateUrl: './server.component.html'
+})
+export class ServerComponent {}
